01a31b39f28e59eeb657f5b3e2fae069c700487e
Lucas Montegú <lmontegu@gmail.com>
2020-08-07 11:49:58 -0300

    docs added

---
+++ docs/precache-manifest.4345e39cdde2c2f0e6be3bedbfb61842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7558ac369af5bd8056c1833ca3690a58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dee6a8d345f31a2b7b9",
+    "url": "/static/css/main.4f0dd0b1.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dee6a8d345f31a2b7b9",
+    "url": "/static/js/main.f95eb330.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
